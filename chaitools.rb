f3261cd3729b00674af68872e0cec540824b47ad
Travis Fischer <travis@chaione.com>
2016-11-04 14:09:20 -0500

    v0.1.0 release of chaitools

---
+++ chaitools.rb
@@ -0,0 +1,25 @@
+class Chaitools < Formula
+  desc "Brew some great software with chaitools"
+  homepage "https://github.com/chaione/chaitools"
+  url "https://github.com/chaione/chaitools/archive/0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "fe9f514f2a05ae7282ba026c7f24e9c46776bb0cf995e19e47e0e35a8c2215e0"
+
+  # bottle do
+  #   root_url "https://github.com/chaione/chaitools/releases/download/0.1.0"
+  #   cellar :any
+  #   sha256 "f7213a4f7e349b15593d5d7fe672f1a125701056903c19b9f7afaeee9a5942ff" => :sierra
+  # end
+
+  depends_on :xcode => ["8.1", :build]
+
+  def install
+    system "ln", "-s", "/usr/local/Homebrew/Library/Homebrew/shims/super/clang", "."
+    system "swift", "build", "-c", "release"
+    bin.install ".build/release/chaitools"
+  end
+
+  test do
+    system "chaitools"
+  end
+end
